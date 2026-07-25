d019e29d091ad31f883cefdac30f14bc55c2f6be
ahmedoubeedou <ahmedoumedbeedou@gmail.com>
2026-07-25 23:05:13 +0000

    Updates

--- src/services/productApi.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-const pathUrl = "https://api.escuelajs.co/api/v1";
-export async function getAllProduct()
-{
-let response = await axios.get(`${pathUrl}/products`);
-return response.data;
-}
